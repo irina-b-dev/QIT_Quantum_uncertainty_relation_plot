5d93fad994d3b45da510287036ea154e834b5690
alemusat <alexandra.musat2905@gmail.com>
2024-05-16 16:54:28 +0300

    please work

--- test.py
+++ test.py
@@ -5,20 +5,48 @@ import numpy as np
 # ro = p|r><r| + (1-p)|r_(perpendicular)><r_(perpendicular)|
 # Purity = Tr[ro^2] = 2p^2 - 2p + 1
 
+def p_entropy_func(p):
+    if p == 0 or p == 1:
+        return 0
+    return -p * math.log2(p) - (1-p) * math.log2(1-p)
+
+def p_search(H_ro):
+    error = 0.001
+    p_list = [i/100000 for i in range(0, 50000)]
+    init = 0
+    fin = len(p_list) - 1
+    while fin >= init:
+        mid = init + (fin - init) // 2
+        p = p_list[mid]
+        entropy_diff = p_entropy_func(p) - H_ro
+        if abs(entropy_diff) < error:
+            return p
+        elif entropy_diff > 0:
+            fin = mid - 1
+        else:
+            init = mid + 1
+    return -1
 
 def purity_calculator(H_ro):
     if H_ro == 0:
-        return 1
-    elif H_ro == 0.3:
-        p = 0.053239
-        #p = 0.946761
-    elif H_ro == 0.6:
-        #p = 0.853898
-        p = 0.146102
-    elif H_ro == 0.9:
-        p = 0.316019
-        # p = 0.683981
-    return 2*(p**2) - 2*p +1
+        return 1, 0.999
+    else:
+        p = p_search(H_ro)
+        return 2*(p**2) - 2*p +1, p
+
+#def purity_calculator(H_ro):
+#    if H_ro == 0:
+#        return 1
+#    elif H_ro == 0.3:
+#        p = 0.053239
+#        #p = 0.946761
+#    elif H_ro == 0.6:
+#        #p = 0.853898
+#        p = 0.146102
+#    elif H_ro == 0.9:
+#        p = 0.316019
+#        # p = 0.683981
+#    return 2*(p**2) - 2*p +1
 
 def p_calculator(H_ro):
     if H_ro == 0:
@@ -68,15 +96,32 @@ def lower_bound_eq13(H_ro, c):
             binary_entropy.append(0)
     return binary_entropy
 
-def numerical_optimum(H_ro, c):
-    p = p_calculator(H_ro)
-    binary_entropy = []
+def entropy_calculator(x, p):
+    #p = p_calculator(H_ro)
+    #p = p_search(H_ro)
+    value = x*p + (1-x)*(1-p)
+    entropy = - value * math.log2(value) - (1-value) * math.log2(1-value)
+    entropy_p = -p * math.log2(p) - (1-p) * math.log2(1-p)
+    return entropy - entropy_p
+
+
+def numerical_optimum(c, p):
+    num_optim = []
     for elem in c:
-        value = p * math.sqrt(elem) + (1-p) * (1-math.sqrt(elem))
-        bn_entr1 = -value*math.log2(value)-(1-value)*math.log2(1-value)
-        bn_entr2 = -p*math.log2(p)-(1-p)*math.log2(1-p)
-        binary_entropy.append(bn_entr1-bn_entr2)
-    return binary_entropy
+        gamma = math.acos(2*elem - 1)
+        fmin = 5
+        alfa_list = [i/100 for i in range(int((gamma/2)*100), int(gamma*100)+1)]
+        for alfa in alfa_list:
+            x1 = (math.cos(alfa) + 1)/2 
+            x2 = (math.cos(gamma - alfa)+1)/2
+            entropy_1 = entropy_calculator(x1, p)
+            entropy_2 = entropy_calculator(x2, p)
+            f = entropy_1 + entropy_2
+            if f < fmin:
+                fmin = f 
+        num_optim.append(fmin)   
+    return num_optim 
+
 
 def l1_entropy(H_ro, P, c):
     l1 = []
@@ -89,18 +134,18 @@ def l1_entropy(H_ro, P, c):
 c = [i/1000 for i in range(500, 1000)]
 h_ro = [0, 0.3, 0.6, 0.9]
 for H_ro in h_ro:
-    P = purity_calculator(H_ro)
+    P, p = purity_calculator(H_ro)
     lb4 = lower_bound_eq4(H_ro, c)
     lb12 = lower_bound_eq12(H_ro, c)
     lb13 = lower_bound_eq13(H_ro, c)
     lb14 = lower_bound_eq14(P, H_ro, c)
-    #no = numerical_optimum(H_ro, c)
+    no = numerical_optimum(c, p)
 
     plt.plot(c, lb4, label = "Eq. (4)")
     plt.plot(c, lb12, label = "Eq. (12)")
     plt.plot(c, lb13, label = "Eq. (13)")
     plt.plot(c, lb14, label = "Eq. (14)")
-    #plt.plot(c, no, label = "Numerical optimum")
+    plt.plot(c, no, label = "Numerical optimum")
 
     plt.xlabel('c')
     plt.ylabel('Lower bound')
@@ -109,7 +154,7 @@ for H_ro in h_ro:
     plt.show()
 
 H_ro = 0.6
-P = purity_calculator(H_ro)
+P, p = purity_calculator(H_ro)
 num_rows = 25
 num_cols = 20
 lb4 = lower_bound_eq4(H_ro, c)
